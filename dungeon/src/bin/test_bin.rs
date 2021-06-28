413c113eab2d26804b72b0806e9e4223caa28723
Ben <benluelo@hotmail.com>
2021-06-27 21:35:59 -0400

    WIP

--- dungeon/src/bin/test_bin.rs
+++ dungeon/src/bin/test_bin.rs
@@ -2,15 +2,12 @@ use std::{convert::TryInto, num::NonZeroU16};
 
 use game::{Dungeon, DungeonType};
 
-#[test]
-fn test_dungeon_creation() {
+fn main() {
     let d = Dungeon::new(
         100.try_into().unwrap(),
         150.try_into().unwrap(),
         NonZeroU16::new(10).unwrap(),
         DungeonType::Cave,
-        true,
+        false,
     );
-
-    std::fs::write("dungeon_test.gif", d.to_gif()).unwrap();
 }
